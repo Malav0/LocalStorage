67c3af32a9495729ca235de0affd7c3a65422f60
Malav <malavfultariya007@gmail.com>
2021-12-29 11:50:45 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LocalStorage';
+  fixed:any=50
+  selectvalue:any
+  fixednum:any
+  select:any
+  getvalue:any
+  
+
+  
+  
+  constructor(){
+
+  }
+
+  setData(){
+    localStorage.setItem("value",JSON.stringify(this.fixed))
+  }
+
+  addData(){
+    console.log(this.selectvalue)
+     this.fixed =  JSON.parse(localStorage.getItem('value') || '[]');
+     this.fixednum = Number(this.fixed)
+     this.select= Number(this.selectvalue)
+     this.fixednum = this.select + this.fixednum
+     console.log(this.fixednum)
+    localStorage.setItem("value",JSON.stringify(this.fixednum));
+    
+  }
+
+  updateData(){
+    console.log(this.selectvalue)
+    this.fixed =  JSON.parse(localStorage.getItem('value') || '[]');
+    this.fixednum = Number(this.fixed)
+    this.select= Number(this.selectvalue)
+    this.fixednum = this.select
+    console.log(this.fixednum)
+    localStorage.setItem("value",JSON.stringify(this.fixednum));
+    
+  }
+  
+  deleteData(){
+    
+    JSON.parse(localStorage.getItem('value') || '[]');
+    localStorage.removeItem('value');
+  }
+
+  getData(){
+    this.getvalue=JSON.parse(localStorage.getItem('value') || '[]');
+    console.log(this.getvalue)
+  }
 }
